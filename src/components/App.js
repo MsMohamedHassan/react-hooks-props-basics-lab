787e1f0998478ecd31469530b6a2501fcc4bf24c
Zamzam <Zamzamhassan44@gmail.com>
2022-07-13 23:20:31 +0300

    props basics lab

--- src/components/App.js
+++ src/components/App.js
@@ -12,7 +12,8 @@ function App() {
     <div>
       <NavBar />
       <Home />
-      <About />
+      <About/>
+      
     </div>
   );
 }
---
+++ src/components/links.js
@@ -0,0 +1,13 @@
+
+function Links(){
+return(
+    <>
+<h3>Links</h3>
+<a href={props.github}></a>
+<a href={props.linkedIn}></a>
+
+    </>
+)
+}
+
+
